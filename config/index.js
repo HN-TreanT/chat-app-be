54f93e4dd4283d7960896cb309e875fca9fdca86
HN-TreanT <hntreant23@gmail.com>
2023-07-20 19:06:46 +0700

    update config db

--- config/index.js
+++ config/index.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 async function connect() {
   try {
     // await mongoose.connect("mongodb://localhost:27017/chat-app");
-    await mongoose.connect(`${process.env.MONGO_URI}`);
+    mongoose.set("strictQuery", false);
+    // await mongoose.connect(`${process.env.MONGO_URI}`);
+    await mongoose.connect(
+      "mongodb+srv://hntreant23:hnam23012002@chat-app.d0hygrq.mongodb.net/chat-app"
+    );
     console.log("connect succeeded");
   } catch (e) {
     console.log("connect fail");
